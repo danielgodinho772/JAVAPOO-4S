8f2f15b1d8fdfafe4a2faebc007908689dd898cf
Daniel Godinho <danielgodinho772@gmail.com>
2025-08-25 11:28:12 -0300

    Added new files

---
+++ src/main/java/com/example/JAVAPOO_4S/Javapoo4SApplication.java
@@ -0,0 +1,13 @@
+package com.example.JAVAPOO_4S;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Javapoo4SApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Javapoo4SApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/JAVAPOO_4S/Javapoo4SApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JAVAPOO_4S;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Javapoo4SApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
